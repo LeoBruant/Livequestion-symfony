5aff94f80a9e97dbde188d547f05a8cf624e3aa5
Leo Bruant <bruantleo@gmail.com>
2020-11-16 11:42:21 +0100

    maj

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('pages/index.php.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexion()
+    {
+        return $this->render('pages/connexion.php.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
