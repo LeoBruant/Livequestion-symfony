adf8db385af90d3cebf9939005e97cef01468df9
Leo Bruant <bruantleo@gmail.com>
2020-11-16 15:21:19 +0100

    maj

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/home", name="home")
      */
     public function index()
     {
-        return $this->render('pages/index.php.twig', [
+        return $this->render('pages/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
@@ -22,8 +22,38 @@ class DefaultController extends AbstractController
      */
     public function connexion()
     {
-        return $this->render('pages/connexion.php.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('pages/connexion.html.twig', [
+			'controller_name' => 'DefaultController',
         ]);
-    }
+	}
+
+	/**
+     * @Route("/presentation", name="presentation")
+     */
+    public function actualites()
+    {
+		$actualites = [
+			[
+				'titre' => 'Titre',
+				'contenu' => 'Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+				'lien' => 'https://lien.com',
+			],
+
+			[
+				'titre' => 'Titre',
+				'contenu' => 'Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+				'lien' => 'https://lien.com',
+			],
+
+			[
+				'titre' => 'Titre',
+				'contenu' => 'Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+				'lien' => 'https://lien.com',
+			],
+		];
+
+		return $this->render('pages/presentation.html.twig', [
+			'actualites' => $actualites,
+		]);
+	}
 }
